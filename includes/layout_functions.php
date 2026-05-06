1af0de976d49911c8bbe6f409f5a604b7447e3e5
Mubasher-Dev <whatsappbot11@hotmail.com>
2026-05-06 19:18:52 +0500

    Move WhatsApp icon styling to a separate CSS file

--- includes/layout_functions.php
+++ includes/layout_functions.php
@@ -79,7 +79,8 @@ function renderHeader($page_title = 'ADDAAX Premium', $active_page = 'home') {
         if (!empty($favicon)): ?>
         <link rel="icon" href="/images/<?php echo htmlspecialchars($favicon); ?>" type="image/x-icon">
         <?php endif; ?>
-        <link rel="stylesheet" href="/css/modern-directory.css?v=2.1">
+        <link rel="stylesheet" href="/css/modern-directory.css?v=2.2">
+        <link rel="stylesheet" href="/css/whatsapp-icons.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" media="print" onload="this.media='all'">
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Outfit:wght@400;700;900&display=swap" rel="stylesheet">
     </head>
